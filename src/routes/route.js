2d8865da582838c4b3f218888bd10bb0fd9e41cd
BenyounesMehdi <benyounesmahdi1234@gmail.com>
2025-03-20 12:01:50 +0000

    Added the login account get-user logout endpoints

--- src/routes/route.js
+++ src/routes/route.js
@@ -2,6 +2,11 @@ import { Router } from "express";
 import home from "../controllers/home.js";
 import login from "../controllers/login.js";
 import downloadPasswords from "../controllers/downloadPasswords.js";
+import handleLogin from "../controllers/handleLogin.js";
+import { authMiddleware } from "../middlewares/auth.js";
+import { getUser } from "../controllers/getUser.js";
+import account from "../controllers/account.js";
+import logout from "../controllers/logout.js";
 
 const router = Router();
 
@@ -10,5 +15,13 @@ router.get("/", home);
 router.get("/login", login);
 
 router.get("/download-passwords", downloadPasswords);
+
+router.post("/login", handleLogin);
+
+router.get("/account", authMiddleware, account);
+
+router.get("/get-user", getUser);
+
+router.get("/logout", logout);
 
 export default router;
